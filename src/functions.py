e9fc402f5b261c4b47ddfcac87d9ca397c06e4ff
icy-beep <iceghostse@gmail.com>
2026-04-08 15:09:49 +0300

    feat(frequent_element): Реализовал функцию поиска наиболее часто встречающегося числа в списке

--- src/functions.py
+++ src/functions.py
@@ -1,19 +1,23 @@
 import typing
+from itertools import count
 
 IS_EVEN = lambda number: number % 2 == 0
 
-def is_in_range(number):
-    return number <= 2*10**4 and number >= -2*10**4
+def in_range(number, low, high):
+    return low <= number <= high
 
 def sum_even_numbers(array: list[int]) -> int:
     if not isinstance(array, list): raise TypeError()
+
+    lower_limit = -2*10**4
+    upper_limit = 2*10**4
 
     total = 0
 
     for number in array:
         if type(number) is bool:
             raise TypeError()
-        if not is_in_range(number):
+        if not in_range(number, lower_limit, upper_limit):
             raise ValueError
 
         if IS_EVEN(number):
@@ -23,3 +27,34 @@ def sum_even_numbers(array: list[int]) -> int:
         return 1
 
     return total
+
+
+def frequent_element(array: list[int]) -> int:
+    if not isinstance(array, list):  raise TypeError()
+    if not array:  return 0
+
+    lower_limit = 1
+    upper_limit = 2*10**4
+    counts = {}
+
+    for number in array:
+        if type(number) is bool:
+            raise TypeError()
+        if not in_range(number, lower_limit, upper_limit):
+            raise ValueError()
+
+        counts[number] = counts.get(number, 0) + 1
+
+    max_frequency = 0
+    leader = None
+
+    for number, frequency in counts.items():
+        if frequency > max_frequency:
+            max_frequency = frequency
+            leader = number
+
+        elif frequency == max_frequency:
+            if number < leader:
+                leader = number
+
+    return leader if leader is not None else 0
